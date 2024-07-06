b5c0211fc4a6509d8277ee189a9aad43c01a85a0
Nindtz <nindita@gmail.com>
2024-07-06 19:55:21 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var playerInstance = jwplayer("jwplayerDiv");
+
+
+
+playerInstance.setup({
+      file: "https://ssc2-ak.akamaized.net/out/v1/a16db2ec338a445a82d9c541cc9293f9/index.mpd",
+      type: "dash",
+      drm: {
+        "clearkey": {
+          "keyId": "8bcfc55359e24bd7ad1c5560a96ddd3c",
+          "key": "b5dcf721ab522af92a9d3bf0bd55b596"
+        }
+      }
+    });
