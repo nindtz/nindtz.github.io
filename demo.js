863ef68b2bf10da6100783f13abdd102eaa240c3
Nindtz <nindita@gmail.com>
2024-07-06 19:20:39 +0700

    Creating

---
+++ demo.js
@@ -0,0 +1,16 @@
+var config = {
+  key: '<YOUR PLAYER KEY>',
+  cast: {
+    enable: true
+  }
+};
+
+var source = {
+  dash: 'https://ssc2-ak.akamaized.net/out/v1/a16db2ec338a445a82d9c541cc9293f9/index.mpd',
+  drm: {
+    "clearkey": {
+      "keyId": "8bcfc55359e24bd7ad1c5560a96ddd3c",
+      "key": "b5dcf721ab522af92a9d3bf0bd55b596"
+    }
+  }
+};
