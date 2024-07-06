8ef27d110e1d64bb69952bb9a35ef1480a826f0d
Nindtz <nindita@gmail.com>
2024-07-06 23:52:03 +0700

    coba cubmu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var playerInstance = jwplayer("jwplayerDiv");
 
 
-
+/*
 playerInstance.setup({
       file: "https://melon-live-cdn.mncnow.id/live/eds/MNCSports3-HD/sa_dash_vmx/MNCSports3-HD.mpd",
       image: "images/video.jpg",
@@ -18,6 +18,18 @@ playerInstance.setup({
           }
     });
 
+*/
+
+playerInstance.setup({
+      file: "https://cdnjkt4.transvision.co.id:1000/live/master/4/4028c6856b8d2390016b9c3ee6c21d7c/manifest.mpd",
+      image: "images/video.jpg",
+      type: "dash",
+      "drm": {
+      "widevine": {
+            "url": "https://lic-cubmux.konslet.workers.dev/4rr0w/play.wv"
+            }
+          }
+    });
 
 /*
 playerInstance.setup({
