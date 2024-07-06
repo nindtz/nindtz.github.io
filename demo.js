b1be80dfbd1033df226561b400a295c4c4eaeba8
Nindtz <nindita@gmail.com>
2024-07-06 19:30:50 +0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -7,10 +7,10 @@ var config = {
 
 var source = {
   dash: 'https://ssc2-ak.akamaized.net/out/v1/a16db2ec338a445a82d9c541cc9293f9/index.mpd',
-  drm: {
-    "clearkey": {
-      "keyId": "8bcfc55359e24bd7ad1c5560a96ddd3c",
-      "key": "b5dcf721ab522af92a9d3bf0bd55b596"
+  'drm': {
+    'clearkey': {
+      'keyId': '8bcfc55359e24bd7ad1c5560a96ddd3c',
+      'key': 'b5dcf721ab522af92a9d3bf0bd55b596'
     }
   }
 };
