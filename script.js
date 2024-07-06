d37b1ab944d0bcfa66e49333c65352279a18e1d4
Nindtz <nindita@gmail.com>
2024-07-06 22:46:40 +0700

    testing widevine

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ var playerInstance = jwplayer("jwplayerDiv");
 
 
 playerInstance.setup({
+      file: "https://melon-live-cdn.mncnow.id/live/eds/MNCSports3-HD/sa_dash_vmx/MNCSports3-HD.mpd",
+      image: "images/video.jpg",
+      type: "dash",
+      "drm": {
+            "widevine": {
+            "url": "https://mrpw.ptmnc01.verspective.net/?deviceId=NzEwNGYwNjEtNzBjNC0zNmJjLTkwZGMtMGQ1Yjg3ZWJhZTBi"
+        }
+      }
+    });
+
+
+/*
+playerInstance.setup({
       file: "https://ssc2-ak.akamaized.net/out/v1/a16db2ec338a445a82d9c541cc9293f9/index.mpd",
       image: "images/video.jpg",
       type: "dash",
@@ -13,3 +26,4 @@ playerInstance.setup({
         }
       }
     });
+*/
