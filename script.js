a8f67ec9f48c169e6f82ea9372f3c53d6d495f84
Nindtz <nindita@gmail.com>
2024-07-07 08:20:00 +0700

    Add alternatives

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ playerInstance.setup({
       }
     });
 
+  function switchToDRM1() {
+    playerInstance.setup({
+      file: "https://bpprod5linear.akamaized.net/bpk-tv/irdeto_com_Channel_252/output/manifest.mpd",
+      type: "dash",
+      drm: {
+        "clearkey": {
+          "keyId": "6a9e4204f3f8577ebf6e79b3b18573f8",
+          "key": "5a8964c25a3a6eba7319f8e4c7efa3aa"
+        }
+      }
+    });
+  }
+
+  // Function to switch to DRM 2
+  function switchToDRM2() {
+    playerInstance.setup({
+      file: "https://bpprod5linear.akamaized.net/bpk-tv/irdeto_com_Channel_250/output/manifest.mpd",
+      type: "dash",
+      drm: {
+        "clearkey": {
+          "keyId": "06341bf3dd635e6ca5c71188d0210373",
+          "key": "201e9857b48590ef73291e35f182a604"
+        }
+      }
+    });
+  }
