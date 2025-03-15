855db65267a867e68ee2e3a936faa32f89fb6a38
Nindtz <nindita@gmail.com>
2025-03-15 13:42:43 +0700

    test audio track

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function switchTo2() {
 
   function switchTo4() {
       playerInstance.setup({
-      file: "https://cdnjkt4.transvision.co.id:1001/live/master/4/4028c685651d7bcc01651f0db0bf0097/manifest.mpd",
+      file: "https://cdnjkt4.transvision.co.id:1000/live/master/4/4028c6856b8d2390016b9c3ee6c21d7c/manifest.mpd",
       image: "images/video.jpg",
       type: "dash",
       dash: {
